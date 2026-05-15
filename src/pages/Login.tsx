f39405381e4e030439e5bbc819e7743140e5a5a2
JorgeDiezGracia <diezgraciajorge@gmail.com>
2026-05-15 13:16:44 +0200

    add new login route

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -68,7 +68,7 @@ export default function Login() {
           </button>
         </form>
 
-        <p>Don't have account? <Link to="/register">Register</Link></p>
+        <p>Don't have an account? <Link to="/register">Register</Link></p>
       </div>
     </div>
   );
